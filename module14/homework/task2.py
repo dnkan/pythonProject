ad07fcc7dddd6b6a1a49e578bd33eb670fd7e545
user <dn.kan@yandex.ru>
2023-10-14 23:10:20 +0300

    initial and add tast1,2

---
+++ module14/homework/task2.py
@@ -0,0 +1,21 @@
+
+def summa_numbers(n):
+    summ = 0
+    while n:
+        summ += n % 10
+        n //= 10
+    return summ
+
+def count_number(n):
+    count = 0
+    while n:
+        count += 1
+        n //= 10
+    return count
+
+N = int(input('Введите число: '))
+
+res_summ = summa_numbers(N)
+res_count = count_number(N)
+
+print('Результат вычислений:', res_summ - res_count)
